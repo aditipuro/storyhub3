958b7db16ac6b979259094a8fc2138f1cf9a402c
aditipuro <68671149+aditipuro@users.noreply.github.com>
2021-02-02 18:28:44 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB_hg20HPmlSYtVsfCBO8QwMztWy4GdeuI",
+    authDomain: "storyhub-143ca.firebaseapp.com",
+    projectId: "storyhub-143ca",
+    storageBucket: "storyhub-143ca.appspot.com",
+    messagingSenderId: "236419278904",
+    appId: "1:236419278904:web:4fe73bd27e974efa4d5ecd"
+  };
+
+  firebase.initializeApp(firebaseConfig);
